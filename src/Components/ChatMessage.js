223586ce7b094d96916c0145a86f00ff5477103d
hyvip-ai <rm2932002@gmail.com>
2021-10-04 18:23:57 +0530

    Time Stamp Updated

--- src/Components/ChatMessage.js
+++ src/Components/ChatMessage.js
@@ -5,15 +5,16 @@ import '../styles/chat.css'
 function ChatMessage({msg}) {
 const {text,uid,photoURL,displayName,createdAt} = msg
 
-console.log(photoURL)
+
  const messegeClass =uid === auth.currentUser.uid? "sent" : "received";
     return (
         <div className={`message ${messegeClass}`}>
             <a href={photoURL} target="_blank" rel="noreferrer"><img src={photoURL} alt={uid}/></a>
           
             <p>{displayName} <br/>
-                {text} <br />
-                {moment(createdAt.seconds*1000).format('MMMM Do YYYY, h:mm:ss a')}
+                {text}
+                <br />
+                {createdAt?moment(createdAt.seconds*1000).format('MMMM Do YYYY, h:mm:ss a'):null}
             </p>
         </div>
     )
